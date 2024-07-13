3e46264368f3b116c2b73d8bf2f0c94654253121
Hendrick Silva <hendrick.fsilvs@gmail.com>
2024-07-13 13:50:43 -0300

    proejto finalizado

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        //TODO: Conhecer e importar a classe Scanner
+        Scanner scanner = new Scanner(System.in);
+        
+        String agencia = "067-8";
+        String nomeCliente = "MARIO ANDRADE";
+        double saldo = 237.48;
+
+        //Exibir as mensagens para o nosso usuário
+        //Obter pela scanner os valore digitador no terminal
+        System.out.println("Por favor, digite o número da sua conta!");
+        System.out.print("Número da conta: ");
+        int numero = scanner.nextInt();
+
+        //Exibir a mensagem conta criada
+        System.out.println("olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo R$" + saldo + " já esta disponível para saque.");
     }
 }
